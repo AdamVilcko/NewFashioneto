a08d149fcf6870fec613dffed39cc3e9d4dcf52d
Caspar <casparss>
2014-05-20 22:32:38 +0100

    Half finished followers/following

---
+++ client/app/views/people/followersfollowing.js
@@ -0,0 +1,28 @@
+define(function(require){
+
+	var
+
+	_              = require("_"),
+
+	Collection     = require("collections/people/people"),
+	Person         = require("views/people/person"),
+	MasterBaseView = require("views/masterbaseview");
+
+
+	return MasterBaseView.extend({
+
+		modelView: Person,
+
+		init: function(){
+			this.collection = new Collection();
+			App.vent.on( "profile:dataLoaded", this.update, this );
+		},
+
+		update: function( data ){
+			this.collection.add( data.followersWrapper.collection );
+			this.renderCollection();
+		}
+
+	});
+
+});
