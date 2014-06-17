9f1aebecb02b9b60e044c5d04f9e797868056bd1
Caspar <casparss>
2014-06-17 20:18:32 +0100

    Adding last files

---
+++ client/app/views/items/itemsearch.js
@@ -0,0 +1,26 @@
+define(function(require){
+
+	var
+
+	Backbone        = require("backbone"),
+	Handlebars      = require("handlebars"),
+	$               = require("jquery"),
+
+	MasterBaseView  = require( 'views/masterbaseview' ),
+	ItemsCollection = require("collections/items/items"),
+	MetaCollection  = require("collections/items/meta"),
+	ItemView        = require("views/items/item");
+
+
+	return MasterBaseView.extend({
+
+		modelView: ItemView,
+
+		init: function(){
+			this.collection     = new ItemsCollection();
+			this.metaCollection = new MetaCollection();
+		}
+
+	});
+
+});
