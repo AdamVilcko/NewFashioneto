3d79c0bbffbf1437a32aff99afb33c56eb6dceb6
Caspar <casparss>
2014-07-27 20:26:15 +0100

    #11 - Created templates and views

---
+++ client/app/views/login/signup.js
@@ -0,0 +1,40 @@
+define(function(require){
+
+	var
+
+	$              = require( "jquery" ),
+	Handlebars     = require( "handlebars" ),
+	Cookie         = require( "jquery.cookie" ),
+
+	User           = require( "models/user" ),
+	MasterBaseView = require( "views/masterbaseview" ),
+	signup         = require( "text!templates/login/signup.hbr" ),
+	signupForm     = require( "text!templates/login/signup-form.hbr" );
+
+
+	return MasterBaseView.extend({
+
+		el: "document.body",
+
+		templates: {
+			signup: Handlebars.compile( signup ),
+			signupForm: Handlebars.compile( signupForm )
+		},
+
+		init: function(){
+			this.render();
+		},
+
+		render: function(){
+			this.$el
+			.html( this.templates.signup() );
+
+			this.$el
+			.html( this.templates.signupForm() );
+		}
+
+	});
+
+});
+
+
