ad6de528f37717e85986f87560a9629c94f58d7a
Felipe <felipe.tonon@truphone.com>
2014-07-27 16:20:16 +0100

    restricting people response to active users only

--- server/fashioneto/src/main/java/com/fashioneto/service/UserServiceImpl.java
+++ server/fashioneto/src/main/java/com/fashioneto/service/UserServiceImpl.java
@@ -46,7 +46,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getUsers() {
-	return userDAO.findAll();
+	return userDAO.findAllActive();
     }
 
     @Override
