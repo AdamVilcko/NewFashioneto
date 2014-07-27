b1e6ecd0dc1ced2b3fa4bae3536a6dd8c19c46af
Felipe <felipe.tonon@truphone.com>
2014-07-27 13:49:49 +0100

    new image size and some refactoring #18

--- server/fashioneto/src/main/java/com/fashioneto/service/ImageServiceImpl.java
+++ server/fashioneto/src/main/java/com/fashioneto/service/ImageServiceImpl.java
@@ -35,121 +35,111 @@ import com.fashioneto.ws.entities.ImageSize;
  */
 @Service("imageService")
 @Transactional
-public class ImageServiceImpl implements ImageService
-{
-	private static final String PATH = "/image-uploads/";
-	private static final String DEFAULT_EXTENSION = "jpg";
+public class ImageServiceImpl implements ImageService {
+    private static final String PATH = "/image-uploads/";
+    private static final String DEFAULT_EXTENSION = "jpg";
 
-	private String imagesPath = System.getProperty("user.home") + PATH;
+    private String imagesPath = System.getProperty("user.home") + PATH;
 
-	@Autowired
-	private ImageDAO imageDAO;
-	@Autowired
-	private AlbumService albumService;
+    @Autowired
+    private ImageDAO imageDAO;
+    @Autowired
+    private AlbumService albumService;
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Override
-	public int like(User user, Image image)
-	{
-		image.addLiker(user);
-		entityManager.merge(image);
-		return image.getLikedBy().size();
+    @Override
+    public int like(User user, Image image) {
+	image.addLiker(user);
+	entityManager.merge(image);
+	return image.getLikedBy().size();
+    }
+
+    @Override
+    public int dislike(User user, Image image) {
+	image.removeLiker(user);
+	entityManager.merge(image);
+	return image.getLikedBy().size();
+    }
+
+    @Override
+    public Image uploadImage(User user, InputStream fileInputStream, String fileExtension) throws IOException {
+
+	String newFilename = TokenUtils.createTokenImageName(user);
+
+	String imageFullPathName = getFullImagePath(newFilename, ImageSize.STANDARD);
+	Thumbnails.of(fileInputStream).outputFormat(DEFAULT_EXTENSION).scale(1).toFile(imageFullPathName);
+
+	fileInputStream.close();
+
+	File standardImage = new File(imageFullPathName);
+	Thumbnails.of(standardImage).width(ImageSize.WALL.getWidth()).outputFormat(DEFAULT_EXTENSION)
+		.toFile(getFullImagePath(newFilename, ImageSize.WALL));
+
+	Thumbnails.of(standardImage).width(ImageSize.SMALL.getWidth()).outputFormat(DEFAULT_EXTENSION)
+		.toFile(getFullImagePath(newFilename, ImageSize.SMALL));
+
+	Thumbnails.of(standardImage).width(ImageSize.THUMBNAIL.getWidth()).outputFormat(DEFAULT_EXTENSION)
+		.toFile(getFullImagePath(newFilename, ImageSize.THUMBNAIL));
+
+	Image image = new Image();
+	image.setDate(new Date());
+	image.setFilename(newFilename);
+	image.setUser(user);
+	image.setFileExtension(DEFAULT_EXTENSION);
+
+	Album album = albumService.getUploadAlbum(user);
+
+	image.setAlbum(album);
+	image = entityManager.merge(image);
+
+	System.gc();
+
+	return image;
+    }
+
+    private String getFullImagePath(String filename, ImageSize imageSize) {
+	return imagesPath + filename + imageSize.getSufix() + '.' + DEFAULT_EXTENSION;
+    }
+
+    @Override
+    public ByteArrayOutputStream getImageContent(int id, ImageSize size) throws IOException {
+	Image image = entityManager.find(Image.class, id);
+	if (image != null) {
+	    String filePath = imagesPath + image.getFullFilename(size.getSufix());
+	    InputStream inputStream = new FileInputStream(new File(filePath));
+	    BufferedImage buffered = ImageIO.read(inputStream);
+	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    ImageIO.write(buffered, image.getFileExtension(), baos);
+	    buffered.flush();
+	    inputStream.close();
+	    System.gc();
+	    return baos;
 	}
+	return null;
+    }
 
-	@Override
-	public int dislike(User user, Image image)
-	{
-		image.removeLiker(user);
-		entityManager.merge(image);
-		return image.getLikedBy().size();
+    @Override
+    public List<Image> getImages(int userId) {
+	return imageDAO.getImages(userId);
+    }
+
+    @Override
+    public List<Integer> getImageIds(int userId) {
+	List<Integer> ids = new ArrayList<Integer>();
+	List<Image> images = imageDAO.getImages(userId);
+	if (images != null) {
+	    for (Image image : imageDAO.getImages(userId)) {
+		ids.add(image.getId());
+	    }
 	}
+	return ids;
+    }
 
-	@Override
-	public Image uploadImage(User user, InputStream fileInputStream, String fileExtension) throws IOException
-	{
-
-		String newFilename = TokenUtils.createTokenImageName(user);
-		//		saveFile(fileInputStream, imagesPath + newFilename + '.' + fileExtension);
-
-		String imageFullPathName = getFullImagePath(newFilename, ImageSize.STANDARD);
-		Thumbnails.of(fileInputStream).outputFormat(DEFAULT_EXTENSION).scale(1).toFile(imageFullPathName);
-
-		fileInputStream.close();
-		
-		File standardImage = new File(imageFullPathName);
-		Thumbnails.of(standardImage).width(ImageSize.SMALL.getWidth()).outputFormat(DEFAULT_EXTENSION)
-				.toFile(getFullImagePath(newFilename, ImageSize.SMALL));
-
-		Thumbnails.of(standardImage).width(ImageSize.THUMBNAIL.getWidth()).outputFormat(DEFAULT_EXTENSION)
-				.toFile(getFullImagePath(newFilename, ImageSize.THUMBNAIL));
-
-		Image image = new Image();
-		image.setDate(new Date());
-		image.setFilename(newFilename);
-		image.setUser(user);
-		image.setFileExtension(DEFAULT_EXTENSION);
-
-		Album album = albumService.getUploadAlbum(user);
-
-		image.setAlbum(album);
-		image = entityManager.merge(image);
-
-		System.gc();
-		
-		return image;
-	}
-
-	private String getFullImagePath(String filename, ImageSize imageSize)
-	{
-		return imagesPath + filename + imageSize.getSufix() + '.' + DEFAULT_EXTENSION;
-	}
-
-	@Override
-	public ByteArrayOutputStream getImageContent(int id, ImageSize size) throws IOException
-	{
-		Image image = entityManager.find(Image.class, id);
-		if (image != null)
-		{
-			String filePath = imagesPath + image.getFullFilename(size.getSufix());
-			InputStream inputStream = new FileInputStream(new File(filePath));
-			BufferedImage buffered = ImageIO.read(inputStream);
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(buffered, image.getFileExtension(), baos);
-			buffered.flush();
-			inputStream.close();
-			System.gc();
-			return baos;
-		}
-		return null;
-	}
-
-	@Override
-	public List<Image> getImages(int userId)
-	{
-		return imageDAO.getImages(userId);
-	}
-
-	@Override
-	public List<Integer> getImageIds(int userId)
-	{
-		List<Integer> ids = new ArrayList<Integer>();
-		List<Image> images = imageDAO.getImages(userId);
-		if (images != null)
-		{
-			for (Image image : imageDAO.getImages(userId))
-			{
-				ids.add(image.getId());
-			}
-		}
-		return ids;
-	}
-
-	@Override
-	public Image getImage(int imageId)
-	{
-		return entityManager.find(Image.class, imageId);
-	}
+    @Override
+    public Image getImage(int imageId) {
+	return entityManager.find(Image.class, imageId);
+    }
 
 }
--- server/fashioneto/src/main/java/com/fashioneto/ws/rest/ImageRestBean.java
+++ server/fashioneto/src/main/java/com/fashioneto/ws/rest/ImageRestBean.java
@@ -34,113 +34,94 @@ import com.sun.jersey.multipart.FormDataParam;
  */
 @Component
 @Path("/image")
-public class ImageRestBean
-{
-	@Autowired
-	private ImageService imageService;
+public class ImageRestBean {
+    @Autowired
+    private ImageService imageService;
 
-	@GET
-	@Path("raw/{imageId}")
-	public Response getImageStandardSize(@PathParam("imageId")
-	int imageId) throws IOException
-	{
-		return getResponseImageContent(imageId, ImageSize.STANDARD);
+    @GET
+    @Path("raw/{imageId}")
+    public Response getImageStandardSize(@PathParam("imageId") int imageId) throws IOException {
+	return getResponseImageContent(imageId, ImageSize.STANDARD);
+    }
+
+    @GET
+    @Path("raw/{imageSize}/{imageId}")
+    public Response getImageStandardSize(@PathParam("imageSize") ImageSize imageSize, @PathParam("imageId") int imageId)
+	    throws IOException {
+	return getResponseImageContent(imageId, imageSize);
+    }
+
+    private Response getResponseImageContent(int imageId, ImageSize imageSize) throws IOException {
+	ByteArrayOutputStream baos = imageService.getImageContent(imageId, imageSize);
+	if (baos != null) {
+	    byte[] imageData = baos.toByteArray();
+	    return Response.ok(imageData).type(new MediaType("image", MediaType.WILDCARD)).build();
+	}
+	return Response.status(Status.NOT_FOUND).build();
+    }
+
+    @POST
+    @Path("upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(@FormDataParam("file") InputStream fileInputStream,
+	    @FormDataParam("file") FormDataContentDisposition contentDispositionHeader,
+	    @FormDataParam("file") FormDataBodyPart body) throws IOException {
+	
+	if (fileInputStream == null || contentDispositionHeader == null || body == null) {
+	    return Response.status(Status.BAD_REQUEST).build();
+	}
+	try {
+	    User user = ContextUtils.getUserFromAuthenticationContext();
+	    Image image = imageService.uploadImage(user, fileInputStream, body.getMediaType().getSubtype());
+	    return Response.ok().entity(FashionetoJsonFactory.getJsonFromObject(image)).build();
+	} catch (NoUserInContextException e) {
+	    e.printStackTrace();
+	    return Response.status(Status.FORBIDDEN).build();
 	}
 
-	@GET
-	@Path("raw/{imageSize}/{imageId}")
-	public Response getImageStandardSize(@PathParam("imageSize")
-	ImageSize imageSize, @PathParam("imageId")
-	int imageId) throws IOException
-	{
-		return getResponseImageContent(imageId, imageSize);
+    }
+
+    @POST
+    @Path("like/{imageId}")
+    public Response likeComment(@PathParam("imageId") int imageId) throws NoUserInContextException {
+	if (imageId < 1) {
+	    return Response.status(Status.NOT_ACCEPTABLE).build();
 	}
 
-	private Response getResponseImageContent(int imageId, ImageSize imageSize) throws IOException
-	{
-		ByteArrayOutputStream baos = imageService.getImageContent(imageId, imageSize);
-		if (baos != null)
-		{
-			byte[] imageData = baos.toByteArray();
-			return Response.ok(imageData).type(new MediaType("image", MediaType.WILDCARD)).build();
-		}
-		return Response.status(Status.NOT_FOUND).build();
+	Image image = imageService.getImage(imageId);
+
+	if (image == null) {
+	    return Response.status(Status.NOT_ACCEPTABLE).build();
+	}
+	return like(image, true);
+    }
+
+    @DELETE
+    @Path("like/{imageId}")
+    public Response dislikeComment(@PathParam("imageId") int imageId) throws NoUserInContextException {
+	Image image = imageService.getImage(imageId);
+	return like(image, false);
+    }
+
+    // DUPLICATED FOR ITEMS!! REfactor this shit, you lazy bastard!!
+    private Response like(Image image, boolean isAdding) throws NoUserInContextException {
+	// http://localhost:8080/Fashioneto/as/item/like/443831786
+
+	if (image != null) {
+	    User user = ContextUtils.getUserFromAuthenticationContext();
+
+	    int likesCount;
+	    if (isAdding) {
+		likesCount = imageService.like(user, image);
+	    } else {
+		likesCount = imageService.dislike(user, image);
+	    }
+
+	    LikesWrapper likesWrapper = new LikesWrapper(likesCount, isAdding);
+	    return Response.status(Status.OK).entity(FashionetoJsonFactory.getJsonFromObject(likesWrapper)).build();
 	}
 
-	@POST
-	@Path("upload")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(@FormDataParam("file")
-	InputStream fileInputStream, @FormDataParam("file")
-	FormDataContentDisposition contentDispositionHeader, @FormDataParam("file")
-	FormDataBodyPart body) throws IOException
-	{
-		try
-		{
-			User user = ContextUtils.getUserFromAuthenticationContext();
-			Image image = imageService.uploadImage(user, fileInputStream, body.getMediaType().getSubtype());
-			return Response.ok().entity(FashionetoJsonFactory.getJsonFromObject(image)).build();
-		}
-		catch (NoUserInContextException e)
-		{
-			e.printStackTrace();
-			return Response.status(Status.FORBIDDEN).build();
-		}
-
-	}
-
-	@POST
-	@Path("like/{imageId}")
-	public Response likeComment(@PathParam("imageId")
-	int imageId) throws NoUserInContextException
-	{
-		if (imageId < 1)
-		{
-			return Response.status(Status.NOT_ACCEPTABLE).build();
-		}
-
-		Image image = imageService.getImage(imageId);
-
-		if (image == null)
-		{
-			return Response.status(Status.NOT_ACCEPTABLE).build();
-		}
-		return like(image, true);
-	}
-
-	@DELETE
-	@Path("like/{imageId}")
-	public Response dislikeComment(@PathParam("imageId")
-	int imageId) throws NoUserInContextException
-	{
-		Image image = imageService.getImage(imageId);
-		return like(image, false);
-	}
-
-	// DUPLICATED FOR ITEMS!! REfactor this shit, you lazy bastard!!
-	private Response like(Image image, boolean isAdding) throws NoUserInContextException
-	{
-		// http://localhost:8080/Fashioneto/as/item/like/443831786
-
-		if (image != null)
-		{
-			User user = ContextUtils.getUserFromAuthenticationContext();
-
-			int likesCount;
-			if (isAdding)
-			{
-				likesCount = imageService.like(user, image);
-			}
-			else
-			{
-				likesCount = imageService.dislike(user, image);
-			}
-
-			LikesWrapper likesWrapper = new LikesWrapper(likesCount, isAdding);
-			return Response.status(Status.OK).entity(FashionetoJsonFactory.getJsonFromObject(likesWrapper)).build();
-		}
-
-		return Response.status(Status.NOT_FOUND).build();
-	}
+	return Response.status(Status.NOT_FOUND).build();
+    }
 
 }
